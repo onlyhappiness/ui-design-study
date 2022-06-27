ce3587b06ab13e1cb5be13e0fd503c0dcd024e5d
onlyhappiness <onlyhappiness96@gmail.com>
2022-06-27 23:07:08 +0900

    [ Progress ] Home - MainHomeBanner

--- e-commerce/components/Home/MainHomeBanner.jsx
+++ e-commerce/components/Home/MainHomeBanner.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import { Box, Card, Paper, Typography } from '@mui/material';
+import { Box, Card, CardMedia, Paper, Typography } from '@mui/material';
 
 const MainHomeBanner = () => {
   return (
     <>
       <Card>
         <Box
-          sx={{
-            width: '100%',
-            height: '30vh',
+          style={{
+            position: 'relative',
+            height: '50vh',
           }}
         >
-          나와
+          <CardMedia
+            component='img'
+            image='https://i.pinimg.com/564x/b5/93/1f/b5931f112201d1833dde27b5145a8249.jpg'
+          />
+
+          <Box
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translateX(-50%)',
+            }}
+          >
+            <Typography variant='h4'>E Commerce</Typography>
+          </Box>
         </Box>
       </Card>
     </>
